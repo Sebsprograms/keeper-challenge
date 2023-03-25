9387decb4035de404a64b0087678254fd591aca2
Sebsprograms <sebastianbenad72@gmail.com>
2023-03-25 12:25:10 +0100

    add second challenge as comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 // Challenge 1 complete
+// Challenge 2 Render all the notes inside notes.js as a seperate Note component.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
